e9e7cfed73008cd3169359f0c97b945fd5a25ef0
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-06-27 18:38:23 +0000

    trying to account for bigger tokens

--- project/tokenplacement.c
+++ project/tokenplacement.c
@@ -9,7 +9,6 @@ int	scorecount(m_line *node, int y, int x)
 
 	score = 0;
 	l = 0;
-	// ft_putendl_fd("finding score INIDE", 2);
 	while (l < node->token->sizey)
 	{
 		i = 0;
@@ -30,7 +29,6 @@ void placementoutput(int y, int x)
 	
 	ret = ft_strjoin(ft_itoa(y), " ");
 	ft_strcat(ret, ft_itoa(x));
-	// printf("%d %d\n",y,x );
 	ft_putendl_fd(ret, 2);
 	ft_putendl_fd(ret, 1);
 	free(ret);
@@ -47,24 +45,18 @@ int	tokenplacement(m_line *node)
 	int	tempscore;
 	// char *ret;
 
-	y = 0;
+	y = 0 - node->grid->sizey;
 	score = 0;
 	tempy = 0;
 	tempx = 0;
 	tempscore = 0;
 	while (y < node->grid->sizey)
 	{
-		x = 0;
+		x = 0 - node->grid->sizex;
 		while (x < node->grid->sizex)
 		{
-				// ret = ft_strjoin(ft_itoa(y), "  ");
-				// ft_strcat(ret, ft_itoa(x));
-				// ft_putstr_fd(ret, 2);
-			// ft_putendl_fd(" finding validpos", 2);
 			if(validpos(node, y, x) == 1)
 			{
-				// ft_putendl_fd("FOUND validpos", 2);
-				// ft_putendl_fd("finding score", 2);
 				tempscore = scorecount(node, y, x);
 				if (tempscore > score)
 				{
@@ -90,3 +82,50 @@ int	tokenplacement(m_line *node)
 	}
 	return (1);
 }
+// int	tokenplacement(m_line *node)
+// {
+// 	int	x;
+// 	int	y;
+// 	int	tempx;
+// 	int	tempy;
+// 	int	score;
+// 	int	tempscore;
+// 	// char *ret;
+
+// 	y = 0;
+// 	score = 0;
+// 	tempy = 0;
+// 	tempx = 0;
+// 	tempscore = 0;
+// 	while (y < node->grid->sizey)
+// 	{
+// 		x = 0;
+// 		while (x < node->grid->sizex)
+// 		{
+// 			if(validpos(node, y, x) == 1)
+// 			{
+// 				tempscore = scorecount(node, y, x);
+// 				if (tempscore > score)
+// 				{
+// 					score = tempscore;
+// 					tempx = x;
+// 					tempy = y;
+// 				}
+// 			}
+// 			x++;
+// 		}
+// 		y++;
+// 	}
+// 	if (tempscore > 0)
+// 	{
+// 		node->playable = 1;
+// 		placementoutput(tempy, tempx);
+// 	}
+// 	else if (tempscore == 0)
+// 	{
+// 		node->playable = 0;
+// 		placementoutput(0, 0);
+
+// 	}
+// 	return (1);
+// }
--- project/validpos.c
+++ project/validpos.c
@@ -11,40 +11,39 @@
 /* ************************************************************************** */
 
 #include "filler.h"
-int	returnval(int omp)
+
+int	returnval(int omp, int count, int stars)
 {
-	if (omp != 1)
+	if (omp != 1 || count != stars)
 	{
 		return (0);
 	}
 	return (1);
 }
 
-int	*tobig(m_line *node, int y, int x)
+int	countstars(m_line *node)
 {
 	int	i;
 	int	l;
-	int	*ret;
+	int	count;
 
+	count = 0;
 	i = 0;
 	l = 0;
-	ret = ft_memalloc(sizeof(int)* 2);
 	while (l < node->token->sizey)
+	{
+		i = 0;
+		while (i < node->token->sizex)
 		{
-			i = 0;
-			while (i < node->token->sizex)
+			if (node->token->mdhold[l][i] == '*')
 			{
-				if (node->token->mdhold[l][i] == '*')
-				{
-					ret[0] = y;
-					ret[1] = x;
-					return (ret);
-				}
-				i++;
+				count++;
 			}
-			l++;
+			i++;
 		}
-	return (ret);
+		l++;
+	}
+	return(count);
 }
 
 int	validpos(m_line *node, int y, int x)
@@ -52,26 +51,103 @@ int	validpos(m_line *node, int y, int x)
 	int	i;
 	int	l;
 	int	omp;
-	int	*yx;
+	int	stars;
+	int	count;
+	// int	*yx;
 
 	l = 0;
 	omp = 0;
-	yx = tobig(node, y, x);
-	if (node->token->sizey + yx[0] > node->grid->sizey || yx[1] + node->token->sizex > node->grid->sizex)
-		return (0);
+	stars = countstars(node);
+	// write(1, "X\n", 2);
+	count = 0;
+
 	while (l < node->token->sizey)
 	{
 		i = 0;
 		while (i < node->token->sizex)
 		{
+			ft_putendl_fd("A", 2);
 			if (node->token->mdhold[l][i] == '*' && (node->grid->mdhold[y+l][x+i] == node->piece ||node->grid->mdhold[y+l][x+i] == node->piece - 32))
 				omp++;
+			ft_putendl_fd("B", 2);
 			if (node->grid->mdhold[y+l][x+i] == node->enem || node->grid->mdhold[y+l][x+i] == node->enem -32)
 				return (0);
+			ft_putendl_fd("C", 2);
+			if (node->token->mdhold[l][i] == '*' && (y+l >= 0 && x+i >= 0) && (y+l < node->token->sizey && x+i < node->token->sizex))
+				count++;
+			ft_putendl_fd("X", 2);
 			i++;
 		}
 		l++;
 	}
-	return(returnval(omp));
+	return(returnval(omp, count, stars));
 
 }
+
+
+
+
+// int	returnval(int omp)
+// {
+// 	if (omp != 1)
+// 	{
+// 		return (0);
+// 	}
+// 	return (1);
+// }
+
+// int	*tobig(m_line *node, int y, int x)
+// {
+// 	int	i;
+// 	int	l;
+// 	int	*ret;
+
+// 	i = 0;
+// 	l = 0;
+// 	ret = ft_memalloc(sizeof(int)* 2);
+// 	while (l < node->token->sizey)
+// 		{
+// 			i = 0;
+// 			while (i < node->token->sizex)
+// 			{
+// 				if (node->token->mdhold[l][i] == '*')
+// 				{
+// 					ret[0] = y;
+// 					ret[1] = x;
+// 					return (ret);
+// 				}
+// 				i++;
+// 			}
+// 			l++;
+// 		}
+// 	return (ret);
+// }
+
+// int	validpos(m_line *node, int y, int x)
+// {
+// 	int	i;
+// 	int	l;
+// 	int	omp;
+// 	int	*yx;
+
+// 	l = 0;
+// 	omp = 0;
+// 	yx = tobig(node, y, x);
+// 	if (node->token->sizey + yx[0] > node->grid->sizey || yx[1] + node->token->sizex > node->grid->sizex)
+// 		return (0);
+// 	while (l < node->token->sizey)
+// 	{
+// 		i = 0;
+// 		while (i < node->token->sizex)
+// 		{
+// 			if (node->token->mdhold[l][i] == '*' && (node->grid->mdhold[y+l][x+i] == node->piece ||node->grid->mdhold[y+l][x+i] == node->piece - 32))
+// 				omp++;
+// 			if (node->grid->mdhold[y+l][x+i] == node->enem || node->grid->mdhold[y+l][x+i] == node->enem -32)
+// 				return (0);
+// 			i++;
+// 		}
+// 		l++;
+// 	}
+// 	return(returnval(omp));
+
+// }
